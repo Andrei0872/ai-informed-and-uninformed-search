81b7740fdc47500c96809ad117bdbdd11b6b5255
Andrei Gatej <andreigtj01@gmail.com>
2022-04-12 21:10:22 +0300

    feat(dfs): collect inf about the alg for when a path is found

--- lib/algorithms/dfs.py
+++ lib/algorithms/dfs.py
@@ -1,16 +1,24 @@
 
 from lib.file import DeserializedFile, filter_out_unusable_keys
-from lib.node import Node, generate_successors, get_path_until_root, is_goal_state, serialize_path
+from lib.node import Node, generate_successors, get_path_until_root, is_goal_state
 
 from queue import LifoQueue
 
-def dfs(node: Node, file: DeserializedFile, max_depth = float('inf')) -> str:
+from lib.path import Path
+
+def dfs(node: Node, file: DeserializedFile, max_nr_solutions, on_path_found, max_depth = float('inf')):
   stack = LifoQueue()
 
   stack.put((node, 0))
 
-  res = ""
+  nr_solutions = 0
+
+  max_nr_nodes_in_memory = -1
+  total_computed_successors = 0
+
   while stack.empty() == False:
+    max_nr_nodes_in_memory = max(max_nr_nodes_in_memory, stack.qsize())
+    
     (crt_node, depth) = stack.get()
 
     if crt_node.applied_key != None:
@@ -21,15 +29,21 @@ def dfs(node: Node, file: DeserializedFile, max_depth = float('inf')) -> str:
 
     if is_goal_state(crt_node):
       path = get_path_until_root(crt_node)
-      print("cost: {}\n".format(crt_node.cost))
-      print(serialize_path(path), '\n\n')
-      res += serialize_path(path) + "\n\n"
+      path_idx = nr_solutions
+
+      on_path_found(Path(path_idx, path, max_nr_nodes_in_memory, total_computed_successors))
+
+      nr_solutions += 1
+      if nr_solutions == max_nr_solutions:
+        return
+
       continue
 
     if depth == max_depth:
       continue
 
-    for successor in generate_successors(crt_node, file.keys, file.unfair_key):
-      stack.put((successor, depth + 1))
+    successors = generate_successors(crt_node, file.keys, file.unfair_key)
+    total_computed_successors += len(successors)
 
-  return res
+    for successor in successors:
+      stack.put((successor, depth + 1))
